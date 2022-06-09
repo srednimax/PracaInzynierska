1f9c3489e6d7be053ba01bf9f1e7b8e56a5311d5
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-06-09 22:00:39 +0200

    book and user configuration

---
+++ LibraryDatabase/Configurations/BookConfigurations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+namespace LibraryDatabase.Configurations;
+
+public class BookConfigurations:IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .HasColumnType("int")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Title)
+            .HasColumnName("title")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+
+        builder.Property(x => x.Author)
+            .HasColumnName("author")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+
+        builder.Property(x => x.PublishYear)
+            .HasColumnName("publish_year")
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder.Property(x => x.Genre)
+            .HasColumnName("genre")
+            .HasColumnType("int")
+            .IsRequired();
+    }
+}
---
+++ LibraryDatabase/Configurations/UserConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+namespace LibraryDatabase.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .HasColumnType("int")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Email)
+            .HasColumnName("email")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+        builder.HasIndex(x => x.Email).IsUnique();
+
+        builder.Property(x => x.Password)
+            .HasColumnName("password")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+
+        builder.Property(x => x.PhoneNumber)
+            .HasColumnName("phone_number")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+
+        builder.Property(x => x.FirstName)
+            .HasColumnName("first_name")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+
+        builder.Property(x => x.LastName)
+            .HasColumnName("last_name")
+            .HasColumnType("nvarchar(MAX)")
+            .IsRequired();
+
+        builder.Property(x => x.Role)
+            .HasColumnName("role")
+            .HasColumnType("int")
+            .HasDefaultValue(Role.User);
+
+        builder.Property(x => x.Gender)
+            .HasColumnName("gender")
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder.Property(x => x.Birth)
+            .HasColumnName("birth")
+            .HasColumnType("datetime")
+            .IsRequired();
+    }
+}
