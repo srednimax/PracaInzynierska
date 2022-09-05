6d4f14392a5e3be76306f787e1c5b70a4f1cefd3
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-09-05 13:51:07 +0200

    remove unused project

--- Services/Interfaces/IUserInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using LibraryBackend.Dtos;
-
-namespace Services.Interfaces;
-
-public interface IUserService
-{
-    public Task<UserDto> SignIn(UserSignInDto userSignInDto);
-    public Task<UserDto> SignUp(UserSignUpDto userSignUpDto);
-}
