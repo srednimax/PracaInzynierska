c8e5dc9c6208ead42c571a70456cc1e82e475fcd
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-09-08 12:40:53 +0200

    jwt token

---
+++ LibraryBackend/Authentication/JwtAuth.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using LibraryDatabase.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LibraryBackend.Authentication;
+
+public class JwtAuth : IAuth
+{
+    private readonly string _key;
+    public JwtAuth(string key)
+    {
+        _key = key;
+    }
+    public string Authentication(User user)
+    {
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenKey = Encoding.ASCII.GetBytes(_key);
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(
+                new[]
+                {
+                    new Claim("id", user.Id.ToString())
+                }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        tokenDescriptor.Subject.AddClaim(new Claim("role", user.Role.ToString()));
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
