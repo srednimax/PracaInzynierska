09b04744af98583fd662eceb20d7c1d2204a1786
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-09-09 12:07:32 +0200

    CRUD book repository

--- LibraryBackend/Repositories/BookRepository.cs
+++ LibraryBackend/Repositories/BookRepository.cs
@@ -1,9 +1,34 @@
 ﻿using LibraryBackend.Repositories.Interfaces;
 using LibraryDatabase.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryBackend.Repositories;
 
-public class BookRepository: Repository<BookRepository>,IBookRepository
+public class BookRepository: Repository<Book>,IBookRepository
 {
     public BookRepository(LibraryDatabaseContext context) : base(context) { }
+    public async Task<Book?> GetBookById(int id)
+    {
+        return await GetAll().FirstOrDefaultAsync(x=>x.Id == id);
+    }
+
+    public async Task<List<Book>> GetAllBooks()
+    {
+        return await GetAll().ToListAsync();
+    }
+
+    public async Task<Book> AddBook(Book book)
+    {
+        return await AddAsync(book);
+    }
+
+    public async Task<Book> UpdateBook(Book book)
+    {
+        return await UpdateAsync(book);
+    }
+
+    public async Task<Book> RemoveBook(Book book)
+    {
+        return await RemoveAsync(book);
+    }
 }
