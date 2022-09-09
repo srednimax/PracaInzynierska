8b30703ddb6ce54dc06c55d409ca1036e05e0489
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-09-09 13:05:34 +0200

    book controller

--- LibraryBackend/Controllers/BookController.cs
+++ LibraryBackend/Controllers/BookController.cs
@@ -20,13 +20,54 @@ namespace LibraryBackend.Controllers
 
         [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<List<BookDto>>> GetAll()
+        public async Task<ActionResult<List<BookDto>>> GetAllBooks()
         {
             var result = await _bookService.GetAllBooks();
 
             return result.Status switch
             {
                 200 => result.Body
+            };
+        }
+
+        [Authorize(Roles = "Employee")]
+        [HttpPost]
+        public async Task<ActionResult<BookDto>> AddBook(BookAddDto bookAddDto)
+        {
+            var result = await _bookService.AddBook(bookAddDto);
+
+            return result.Status switch
+            {
+                200 => result.Body,
+                500 => Problem(result.Message)
+            };
+        }
+
+        [Authorize(Roles = "Employee")]
+        [HttpPut]
+        public async Task<ActionResult<BookDto>> UpdateBook(BookUpdateDto bookUpdateDto)
+        {
+            var result = await _bookService.UpdateBook(bookUpdateDto);
+
+            return result.Status switch
+            {
+                200 => result.Body,
+                404 => NotFound(),
+                500 => Problem(result.Message)
+            };
+        }
+
+        [Authorize(Roles = "Employee")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BookDto>> RemoveBook(int id)
+        {
+            var result = await _bookService.RemoveBook(id);
+
+            return result.Status switch
+            {
+                200 => result.Body,
+                404 => NotFound(),
+                500 => Problem(result.Message)
             };
         }
 
