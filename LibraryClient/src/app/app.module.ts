8219f0fac1629b6186b4abe05806d0bbc8b564d1
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-10-14 11:40:45 +0200

    bootstrap

--- LibraryClient/src/app/app.module.ts
+++ LibraryClient/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+//primeng
+import {InputTextModule} from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ LibraryClient/src/app/services/JwtInteceptor.ts
@@ -0,0 +1,66 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class JwtInterceptor implements HttpInterceptor {
+  private readonly token: string | null;
+
+  constructor(private _router: Router) {
+    this.token = localStorage.getItem("token");
+  }
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (this.token) {
+      const modReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+      return next.handle(modReq).pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMsg = "";
+          if (error.error instanceof ErrorEvent) {
+            errorMsg = `Error: ${error.error.message}`;
+          } else {
+            errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+                   
+          }
+          if (error.status == 401 || error.status == 403)
+          { 
+             this._router.navigate(["/"]);
+          }
+          return throwError(errorMsg);
+        })
+      );
+    }
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMsg = "";
+        if (error.error instanceof ErrorEvent) {
+          errorMsg = `Error: ${error.error.message}`;
+        } else {
+          errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+        }
+        if (error.status == 401 || error.status == 403)
+         { 
+            this._router.navigate(["/"]);
+         }
+        return throwError(errorMsg);
+      })
+    );
+  }
+}
